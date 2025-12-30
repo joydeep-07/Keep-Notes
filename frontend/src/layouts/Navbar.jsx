3ca945f6e0f1ca6c3bf8a170df2a08ca3efe52c9
Joydeep Paul <joydeeprnp8821@gmail.com>
2025-12-30 23:27:11 +0530

    Updated

--- frontend/src/layouts/Navbar.jsx
+++ frontend/src/layouts/Navbar.jsx
@@ -23,7 +23,7 @@ const Navbar = () => {
           : "bg-gradient-to-r from-transparent via-transparent to-transparent"
       }`}
     >
-      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+      <div className=" mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex items-center justify-between h-16">
           {/* Logo */}
           <div className="flex justify-center flex-col">
