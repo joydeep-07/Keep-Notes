fa94c68aec311bc263ac1a6f7f635d0ed0711581
Joydeep Paul <joydeeprnp8821@gmail.com>
2025-12-30 23:09:59 +0530

    Theme Toggling

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect } from "react";
 import { useSelector } from "react-redux";
-
+import ThemeToggle from "./components/ThemeToggle";
 const App = () => {
   const { mode } = useSelector((state) => state.theme);
 
@@ -15,6 +15,7 @@ const App = () => {
   return (
     <>
       <div>
+        <ThemeToggle/>
         <h1 className="text-6xl heading font-light text-center pt-35">Hello World</h1>
       </div>
     </>
