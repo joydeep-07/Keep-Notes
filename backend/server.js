9fecad290b4b5cfc40b651cb752ac3831abb7dee
Joydeep Paul <joydeeprnp8821@gmail.com>
2026-01-01 11:30:19 +0530

    Backend Auth Logic Working

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,21 @@
 const express = require("express");
+const dotenv = require("dotenv");
+const cors = require("cors");
+const connectDB = require("./db/db");
+
+dotenv.config();
+connectDB();
+
 const app = express();
-const port = 3000;
+
+app.use(cors());
+app.use(express.json());
+
+app.use("/api/users", require("./routes/User.route"));
 
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.send("API running...");
 });
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
-});
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
